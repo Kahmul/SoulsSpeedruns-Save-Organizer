2e2bafba7f91462b95d271698e69218272b6b2c4
Kahmul <kahmul78@web.de>
2023-12-13 11:20:58 +0100

    Adjust context menu icons

--- src/com/soulsspeedruns/organizer/savelist/SaveListContextMenu.java
+++ src/com/soulsspeedruns/organizer/savelist/SaveListContextMenu.java
@@ -93,7 +93,7 @@ public class SaveListContextMenu extends JPopupMenu
 	private JMenuItem createAddItem(SaveList saveList)
 	{
 		JMenuItem itemAdd = new JMenuItem("Add Folder");
-		itemAdd.setIcon(IconFontSwing.buildIcon(Elusive.PLUS_SIGN, 15, new Color(39, 174, 96)));
+		itemAdd.setIcon(IconFontSwing.buildIcon(Elusive.PLUS_SIGN, 15, Color.decode("0x49ad73")));
 		itemAdd.addActionListener(event -> {
 			saveList.askToCreateFolder();
 		});
@@ -109,7 +109,7 @@ public class SaveListContextMenu extends JPopupMenu
 	private JMenuItem createRemoveItem(SaveList saveList)
 	{
 		JMenuItem itemRemove = new JMenuItem("Delete");
-		itemRemove.setIcon(IconFontSwing.buildIcon(Iconic.CHECK, 18, new Color(231, 76, 60)));
+		itemRemove.setIcon(IconFontSwing.buildIcon(Iconic.CHECK, 18, Color.decode("0xee6a5c")));
 		itemRemove.setAccelerator(KeyStroke.getKeyStroke("DELETE"));
 		itemRemove.addActionListener(event -> {
 			saveList.askToDeleteEntries(saveList.getSelectedValuesList());
@@ -126,7 +126,7 @@ public class SaveListContextMenu extends JPopupMenu
 	private JMenuItem createEditItem(SaveList saveList)
 	{
 		JMenuItem itemEdit = new JMenuItem("Rename");
-		itemEdit.setIcon(IconFontSwing.buildIcon(Elusive.EDIT, 15, new Color(243, 156, 18)));
+//		itemEdit.setIcon(IconFontSwing.buildIcon(Elusive.EDIT, 15, new Color(243, 156, 18)));
 		itemEdit.setAccelerator(KeyStroke.getKeyStroke("F2"));
 		itemEdit.addActionListener(event -> {
 			saveList.askToEditEntry(saveList.getSelectedValue());
@@ -156,7 +156,7 @@ public class SaveListContextMenu extends JPopupMenu
 	private JMenuItem createRefreshItem(SaveList saveList)
 	{
 		JMenuItem itemRefresh = new JMenuItem("Refresh From File System");
-		itemRefresh.setIcon(IconFontSwing.buildIcon(Elusive.REPEAT, 15, new Color(138, 43, 226)));
+		itemRefresh.setIcon(IconFontSwing.buildIcon(Elusive.REPEAT, 14, Color.decode("0x4aa4fb")));
 		itemRefresh.addActionListener(event -> {
 			saveList.refresh();
 		});
@@ -170,7 +170,7 @@ public class SaveListContextMenu extends JPopupMenu
 	private JMenuItem createOpenInExplorerItem(SaveList saveList)
 	{
 		JMenuItem itemOpenInExplorer = new JMenuItem("Open Folder In Explorer");
-		itemOpenInExplorer.setIcon(IconFontSwing.buildIcon(Entypo.EXPORT, 15, new Color(30, 144, 255)));
+//		itemOpenInExplorer.setIcon(IconFontSwing.buildIcon(Entypo.EXPORT, 15, new Color(30, 144, 255)));
 		itemOpenInExplorer.addActionListener(event -> {
 			SaveListEntry entry = saveList.getSelectedValue();
 			File dirToOpen = OrganizerManager.getSelectedProfile().getRoot().getFile(); // default folder to open
