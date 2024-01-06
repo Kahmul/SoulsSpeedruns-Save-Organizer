c8c8a2aa64c3493648ae7517bde599c0e26ab84f
Kahmul <kahmul78@web.de>
2024-01-06 13:15:10 +0100

    Improve hyperlink underline implementation

--- src/com/soulsspeedruns/organizer/mainconfig/HyperLink.java
+++ src/com/soulsspeedruns/organizer/mainconfig/HyperLink.java
@@ -3,16 +3,15 @@ package com.soulsspeedruns.organizer.mainconfig;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Desktop;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
 
-import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import com.github.weisj.darklaf.LafManager;
 import com.github.weisj.darklaf.theme.event.ThemeChangeEvent;
@@ -20,7 +19,7 @@ import com.github.weisj.darklaf.theme.event.ThemeChangeListener;
 
 public class HyperLink extends JButton
 {
-
+	
 	public HyperLink(String text, String url)
     {
 		super(text);
@@ -34,6 +33,15 @@ public class HyperLink extends JButton
 		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		
 		setForeground((Color) UIManager.get("hyperlink"));
+		
+		getModel().addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e)
+			{
+				setText(getModel().isRollover() ? "<html><u>" + text + "</u></html>" : text);
+			}
+		});
 		
 		addActionListener(new ActionListener() {
 			
@@ -65,22 +73,4 @@ public class HyperLink extends JButton
 		});
 	}
 
-	
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		
-		FontMetrics fm = g.getFontMetrics();
-		
-        if(getModel().isRollover())
-        {
-        	Icon icon = getIcon();
-        	if(icon != null)
-            	g.drawLine(icon.getIconWidth() + getIconTextGap() + 2, fm.getHeight() + fm.getDescent(), getWidth() - 2, fm.getHeight() + fm.getDescent());
-        	else
-        		g.drawLine(2, fm.getHeight(), getWidth() - 2, fm.getHeight());
-        }
-
-	}
-	
 }
